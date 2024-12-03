98a3a98052d2b295b0a3c058762dbf111f94bc92
arnau_barrero <a23arnbarsor@inspedralbes.cat>
2024-12-03 09:26:26 +0100

    access #001

---
+++ front-vue/src/components/register.vue
@@ -0,0 +1,11 @@
+<script>
+</script>
+
+<template>
+    <form action="">
+        <input type="text" placeholder="">
+    </form>
+</template>
+
+<style scoped>
+</style>
---
+++ front-vue/src/views/access.vue
@@ -0,0 +1,24 @@
+<script>
+import { ref } from "vue";
+
+import Login from "../components/login.vue";
+import Register from "../components/register.vue";
+
+export default {
+    components: {
+        Login,
+        Register,
+    },
+    setup() {
+        return {};
+    },
+};
+</script>
+
+<template>
+    <Login />
+</template>
+
+<style scoped>
+/* Agrega aquí tu estilo personalizado */
+</style>
