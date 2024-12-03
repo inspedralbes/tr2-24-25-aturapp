705cf9278ec60d9cd695d4a416fae0b78bed5d18
benjamin <a23benromdor@inspedralbes.cat>
2024-12-03 12:31:01 +0100

    tables created

--- back/laravel/app/Models/User.php
+++ back/laravel/app/Models/User.php
@@ -18,9 +18,14 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'nom',
+        'cognoms',
+        'dni',
         'email',
         'password',
+        'curs',
+        'torn',
+        'rol',
     ];
 
     /**
