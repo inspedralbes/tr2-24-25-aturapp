89c0011e7c1c04c469bb6a418e95be78a9b8909d
benjamin <a23benromdor@inspedralbes.cat>
2024-12-03 12:43:05 +0100

    arreglo main

---
+++ back/laravel/database/migrations/0001_01_01_000000_create_users_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom');
+            $table->string('cognoms');
+            $table->string('dni');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->unsignedBigInteger('rol')->default(0);
+            $table->foreign('rol')->references('id')->on('rols')->onDelete('cascade');
+            $table->unsignedBigInteger('torn')->default(0);
+            $table->foreign('torn')->references('id')->on('torns')->onDelete('cascade');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+
+        Schema::create('password_reset_tokens', function (Blueprint $table) {
+            $table->string('email')->primary();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+        Schema::dropIfExists('password_reset_tokens');
+    }
+};
