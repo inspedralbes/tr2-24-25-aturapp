c91c14bf473465c4143603213c6e5c84e8d856cf
benjamin <a23benromdor@inspedralbes.cat>
2024-12-05 10:29:20 +0100

    Docker enchega be, no mostra vue

--- back/laravel/script.sh
+++ back/laravel/script.sh
@@ -6,18 +6,20 @@ composer install
 
 # Copiar .env si no existe
 if [ ! -f .env ]; then
+    echo "El archivo .env no existe. Creando uno desde .env.example..."
     cp .env.example .env
 fi
 
 # Generar APP_KEY si no existe
 if ! grep -q "APP_KEY=" .env || [ -z "$(grep 'APP_KEY=' .env | cut -d '=' -f 2)" ]; then
+    echo "Creant la key del env"
     php artisan key:generate
 fi
 
 # Ejecutar migraciones y seed solo si es la primera vez
-if [ ! -f /var/www/.migrated ]; then
+if [ ! -f /var/www/html/.migrated ]; then
     php artisan migrate:fresh --seed
-    touch /var/www/.migrated
+    touch /var/www/html/.migrated
 fi
 
 # Iniciar el servidor
