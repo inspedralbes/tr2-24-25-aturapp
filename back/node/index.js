5eff2252fec43ad97dd15c72ac3476fc71dec41d
benjamin <a23benromdor@inspedralbes.cat>
2024-12-10 09:44:36 +0100

    En process de tenir-ho en dos llocs

--- back/node/index.js
+++ back/node/index.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const { createServer } = require('node:http');
-const { join } = require('node:path');
 const { Server } = require('socket.io');
+const { join } = require('node:path');
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:8000',
+    methods: ['GET', 'POST']
+  }
+});
 
-// app.get('/', (req, res) => {
-//   res.sendFile(join(__dirname, 'index.html'));
-// });
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, 'index.html'));
+});
 
 
 io.on('connection', (socket) => {
--- front/src/components/ChatComp.vue
+++ front/src/components/ChatComp.vue
@@ -1,11 +1,38 @@
 <template>
-    Chat
+    <div>
+        <ul id="misstages" class="mostrar">
+            <li v-for="(msg, index) in messages" :key="index">{{ msg }}</li>>
+        </ul>
+        <input v-model="missatge" autocomplete="off" />
+        <button @click="sendMessage"></button>
+    </div>
 </template>
 
 <script setup>
+import { ref, onMounted, onUnmounted } from 'vue';
+import { io } from 'socket.io-client';
 
+const socket = io('http://localhost:9001');
+
+const messages = ref([]);
+const input = ref('');
+
+function sendMessage(){
+  if (input.value) {
+    socket.emit('sendMessage', input.value); // Emitir mensaje al servidor
+    input.value = ''; // Limpiar el campo de entrada
+  }
+};
+  
+onMounted(() => {
+  socket.on('storeMessage', (msg) => {
+    messages.value.push(msg); // Agregar mensaje recibido a la lista
+  });
+});
+
+onUnmounted(() => {
+  socket.off();
+});
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
