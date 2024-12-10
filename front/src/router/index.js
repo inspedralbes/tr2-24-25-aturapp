9ecaa8266c0e097ab1bc892853532ffd200b3977
arnau_barrero <a23arnbarsor@inspedralbes.cat>
2024-12-10 10:02:57 +0100

    Reedireccions Correctes

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,13 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      // path: '/',
-      // redirect: '/home',
-    },
     {
       path: '/home',
       name: 'home',
@@ -16,24 +11,36 @@ const router = createRouter({
     {
       path: '/',
       name: 'mobile',
-      component: () => import('../views/Mobile.vue')
+      component: () => import('../views/Mobile.vue'),
     },
     {
       path: '/access',
       name: 'access',
-      component: () => import('../views/access.vue')
+      component: () => import('../views/access.vue'),
     },
     {
       path: '/login',
       name: 'login',
-      component: () => import('../components/login.vue')
+      component: () => import('../components/login.vue'),
     },
     {
       path: '/register',
       name: 'register',
-      component: () => import('../components/register.vue')
+      component: () => import('../components/register.vue'),
     },
   ],
+});
+
+router.beforeEach((to, from, next) => {
+  const SessionIniciada = localStorage.getItem('loggedIn') === 'true';
+  const PaginasPublicas = ['/access', '/login', '/register', '/home'];
+  const EsPaginaPublica = PaginasPublicas.includes(to.path);
+
+  if (!SessionIniciada && !EsPaginaPublica) {
+    next('/home');
+  } else {
+    next();
+  }
 });
 
 export default router;
