c048faac43240c0f14a668012c85b994eeb891e7
arnau_barrero <a23arnbarsor@inspedralbes.cat>
2024-12-11 12:12:27 +0100

    api preguntas

--- back/laravel/app/Models/Pregunta.php
+++ back/laravel/app/Models/Pregunta.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pregunta extends Model
 {
-    protected $table = 'pregunta';
+    protected $table = 'preguntas';
 
     protected $primaryKey = 'id';
     protected $fillable = ['pregunta'];
