d02ca72b97de6aada5a8d59e384b9ec8188aa820
a23agunovnov <a23agunovnov@inspedralbes.cat>
2024-12-17 19:11:43 +0100

    Creació vistes admin + gestió de rutes per rols i autentificació

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -14,96 +14,85 @@ const router = createRouter({
       path: '/home',
       name: 'home',
       component: () => import('../components/HomeComp.vue'),
+      meta: { requiresAuth: true, rol: 1 },
     },
     {
       path: '/access',
       name: 'access',
       component: () => import('../views/access.vue'),
+      meta: { public: true },
     },
     {
       path: '/login',
       name: 'login',
       component: () => import('../components/login.vue'),
+      meta: { public: true },
     },
     {
       path: '/register',
       name: 'register',
-      component: () => import('../components/register.vue')
+      component: () => import('../components/register.vue'),
+      meta: { public: true },
     },
     {
       path: '/chat',
       name: 'chat',
-      component: () => import('../components/ChatComp.vue')
+      component: () => import('../components/ChatComp.vue'),
+      meta: { requiresAuth: true, rol: 1 },
     },
     {
       path: '/mapa',
       name: 'mapa',
-      component: () => import('../components/MapComp.vue')
+      component: () => import('../components/MapComp.vue'),
+      meta: { requiresAuth: true, rol: 1 },
     },
     {
       path: '/perfil',
       name: 'perfil',
-      component: () => import('../components/UserComp.vue')
+      component: () => import('../components/UserComp.vue'),
+      meta: { requiresAuth: true, rol: 1 },
     },
     {
       path: '/perfil/alertes',
       name: 'mevesAlertes',
-      component: () => import('../components/MevesAlertesComp.vue')
+      component: () => import('../components/MevesAlertesComp.vue'),
+      meta: { requiresAuth: true, rol: 1 },
     },
     {
       path: '/perfil/dades',
       name: 'dades',
-      component: () => import('../components/DadesUserComp.vue')
+      component: () => import('../components/DadesUserComp.vue'),
+      meta: { requiresAuth: true, rol: 1 },
     },
     {
       path: '/soport',
       name: 'soport',
-      component: () => import('../components/SoportComp.vue')
-    },
-    {
-      path: '/chat',
-      name: 'chat',
-      component: () => import('../components/ChatComp.vue')
-    },
-    {
-      path: '/mapa',
-      name: 'mapa',
-      component: () => import('../components/MapComp.vue')
-    },
-    {
-      path: '/perfil',
-      name: 'perfil',
-      component: () => import('../components/UserComp.vue')
-    },
-    {
-      path: '/perfil/alertes',
-      name: 'mevesAlertes',
-      component: () => import('../components/MevesAlertesComp.vue')
+      component: () => import('../components/SoportComp.vue'),
+      meta: { public: true },
     },
     {
       path: '/perfil/alertes/editar',
       name: 'editarAlerta',
-      component: () => import('../components/EditarAlertaComp.vue')
-    },
-    {
-      path: '/perfil/dades',
-      name: 'dades',
-      component: () => import('../components/DadesUserComp.vue')
-    },
-    {
-      path: '/soport',
-      name: 'soport',
-      component: () => import('../components/SoportComp.vue')
+      component: () => import('../components/EditarAlertaComp.vue'),
+      meta: { requiresAuth: true, rol: 1 },
     },
     {
       path: '/enquesta',
       name: 'enquesta',
-      component: () => import('../components/enquesta.vue')
+      component: () => import('../components/enquesta.vue'),
+      meta: { requiresAuth: true, rol: 1 },
     },
     {
-      path: '/heatmap',
+      path: '/admin/heatmap',
       name: 'heatmap',
-      component: () => import('../components/HeatmapComp.vue')
+      component: () => import('../components/HeatmapComp.vue'),
+      meta: { requiresAuth: true, rol: 2 },
+    },
+    {
+      path: '/admin',
+      name: 'adminHome',
+      component: () => import('../components/HomeAdminComp.vue'),
+      meta: { requiresAuth: true, rol: 2 },
     },
   ],
 });
@@ -112,19 +101,31 @@ router.beforeEach((to, from, next) => {
   const store = useCounterStore();
 
   const SessionIniciada = store.Iniciado;
-  const PaginasPublicas = ['/access', '/login', '/register', '/'];
-  const EsPaginaPublica = PaginasPublicas.includes(to.path);
+  const isAdmin = store.userData?.user.rol === 2;
+  const isUser = store.userData?.user.rol === 1;
 
-  if( (to.path === '/login' || to.path === '/register' || to.path === '/access') && SessionIniciada){
-    next('/home');
-  }
+  const EsPaginaPublica = to.matched.some((record) => record.meta.public);
 
-  if (!SessionIniciada && !EsPaginaPublica) {
+  const RequiereAutenticacion = to.matched.some((record) => record.meta.requiresAuth);
+
+  const RolRequerido = to.meta.rol;
+
+  // Redirección lógica
+  if (!SessionIniciada && RequiereAutenticacion) {
+    // Si no está logueado y la página requiere autenticación
     next('/login');
-  } else if (SessionIniciada && to.path === '/') {
-    next('/home'); // Solo redirige a /home si intentas acceder al landing estando logueado
+  } else if (SessionIniciada && EsPaginaPublica) {
+    // Si ya está logueado, evita acceder a páginas públicas como login o register
+    next(isAdmin ? '/admin' : '/home');
+  } else if (RequiereAutenticacion && RolRequerido) {
+    // Si la página requiere un rol específico
+    if ((RolRequerido === 2 && isAdmin) || (RolRequerido === 1 && isUser)) {
+      next(); // El rol coincide, permite el acceso
+    } else {
+      next('/'); // Redirige al inicio si no tiene el rol correcto
+    }
   } else {
-    next(); // Permite la navegación a cualquier otra ruta
+    next(); // Permite la navegación
   }
 });
 
