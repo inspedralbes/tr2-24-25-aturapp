56b5f402f4f432a6ba5cc6a832a0ebaa5f47dca6
a23agunovnov <a23agunovnov@inspedralbes.cat>
2024-12-17 19:13:59 +0100

    Canvi estetic

--- front/src/components/HomeAdminComp.vue
+++ front/src/components/HomeAdminComp.vue
@@ -1,5 +1,5 @@
 <template>
-    <h2 class="d-flex j-center mt-60">Bienvenido, este es tu panel de administrador</h2>
+    <h2 class="d-flex j-center mt-60">Bienvenido, este es su panel de administrador</h2>
 </template>
 
 <script setup>
