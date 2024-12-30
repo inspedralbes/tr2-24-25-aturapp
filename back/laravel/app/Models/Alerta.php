42052f29738530f5fd031f12f15331650bb1d727
a23agunovnov <a23agunovnov@inspedralbes.cat>
2024-12-30 13:18:15 +0100

    Alerta model merge fixed

--- back/laravel/app/Models/Alerta.php
+++ back/laravel/app/Models/Alerta.php
@@ -1,24 +1,30 @@
 <?php
-    namespace App\Models;
-    use Illuminate\Database\Eloquent\Model;
 
-    class Alerta extends Model {
-        protected $fillable = [
-            'alumno_id',
-            'sector_id',
-            'descripcion',
-            'estado_id'
-        ];
+namespace App\Models;
 
-        public function user() {
-            return $this->belongsTo(User::class, 'alumno_id', 'id');
-        }
+use Illuminate\Database\Eloquent\Model;
 
-        public function sector() {
-            return $this->belongsTo(Sector::class);
-        }
+class Alerta extends Model
+{
+    protected $fillable = [
+        'alumno_id',
+        'sector_id',
+        'descripcion',
+        'estado_id'
+    ];
 
-        public function estado() {
-            return $this->belongsTo(Estado::class, 'estado_id', 'id');
-        }
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'alumno_id', 'id');
     }
+
+    public function sector()
+    {
+        return $this->belongsTo(Sector::class);
+    }
+
+    public function estado()
+    {
+        return $this->belongsTo(Estado::class, 'estado_id', 'id');
+    }
+}
