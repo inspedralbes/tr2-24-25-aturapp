e12b4897015b6e585bdf1c2cb28ec3fb3facbb38
a23agunovnov <a23agunovnov@inspedralbes.cat>
2024-12-30 13:23:07 +0100

    AdminNavbar added "estadisticas"

--- front/src/components/AdminNavbar.vue
+++ front/src/components/AdminNavbar.vue
@@ -1,13 +1,15 @@
 <template>
-    <ul class="d-flex no-style j-around no-margin" style="color: red">
-        <li @click="navigateTo('')">INICI</li>
-        <li @click="navigateTo('/heatmap')">HEATMAP</li>
-        <li @click="navigateTo('/alumnes')">ALUMNES</li>
-        <li @click="navigateTo('/alertes')">ALERTES</li>
-        <li @click="navigateTo('/sociograma')">SOCIOGRAMA</li>
-        <li>Incidencia</li>
-    </ul>
-
+    <div class="d-flex j-center">
+        <ul class="d-flex no-style j-around no-margin" style="color: red; width: 600px;">
+            <li style="cursor: pointer;" @click="navigateTo('')">INICI</li>
+            <li style="cursor: pointer;" @click="navigateTo('/alertes')">ALERTES</li>
+            <li style="cursor: pointer;" @click="navigateTo('/heatmap')">HEATMAP</li>
+            <li style="cursor: pointer;" @click="navigateTo('/alumnes')">ALUMNES</li>
+            <li style="cursor: pointer;" @click="navigateTo('/estadisticas')">ESTADISTICAS</li>
+            <li style="cursor: pointer;" @click="navigateTo('/sociograma')">SOCIOGRAMA</li>
+            <li style="cursor: pointer;" @click="navigateTo('/incidencia')">INCIDENCIA</li>
+        </ul>
+    </div>
 </template>
 
 <script setup>
@@ -20,8 +22,4 @@ function navigateTo(path) {
 }
 </script>
 
-<style scoped>
-    li{
-        color: red;
-    }
-</style>
+<style scoped></style>
