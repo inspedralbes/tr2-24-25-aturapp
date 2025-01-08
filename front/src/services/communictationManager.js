cf809c3723c2d445aa1950c72693017526aab28e
arnau_barrero <a23arnbarsor@inspedralbes.cat>
2025-01-08 11:40:10 +0100

    login.vue

--- front/src/services/communictationManager.js
+++ front/src/services/communictationManager.js
@@ -442,3 +442,26 @@ export const fetchAlertes = async (usuari_id) => {
         throw error;
     }
 };
+
+export const loginUser = async (loginData) => {
+    try {
+        const response = await fetch(`${BASE_URL}/api/login`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            },
+            body: JSON.stringify(loginData),
+        });
+
+        if (!response.ok) {
+            const data = await response.json();
+            throw new Error(data.message || 'Credenciales incorrectas');
+        }
+
+        return await response.json();
+    } catch (error) {
+        console.error("Error en loginUser:", error.message);
+        throw error;
+    }
+};
